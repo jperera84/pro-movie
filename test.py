9437b89dc90ac4dd9cfc7aa976a93eb60a27c83d
Jose R. Perera Morales <jpereramor@gmail.com>
2017-01-27 17:57:08 -0500

    Finished render

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 """ Testing Program """
 import movie
+import render_page
 
-MATRIX = movie.Movie("Matrix", "https://wikipedia.com", "The matrix comment", "https://youtube.com",
-                     "Sciencie Fiction", "Martin Scorceses", "1:55", "PG-13", "1998-01-01")
+MOVIE1 = movie.Movie("The girl on the train",
+                     "https://upload.wikimedia.org/wikipedia/en/3/34/The_Girl_on_The_Train.jpg",
+                     """The next time she rides the train, Rachel sees someone reading
+                     the news on his tablet and the lead story is about a missing woman
+                     named Megan Hipwell, the woman Rachel watches from the train whom
+                     she exited the train to yell at about cheating on her husband.""",
+                     "https://www.youtube.com/watch?v=KkoEE1i0CX8",
+                     "Thriller", "Martin Scorceses", "1:55", "PG-13", "1998-01-01")
 
-print MATRIX.storyline
+MEDIAS = []
+MEDIAS.append(MOVIE1)
+render_page.open_media_page(MEDIAS)
